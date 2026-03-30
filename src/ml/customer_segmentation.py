974851c963d1de32a411b115530dde3bcbc296b1
Nirdosh Gandhi <gandhinirdosh@gmail.com>
2026-03-30 22:13:46 +0530

    feat: add customer segmentation task to pipeline with KMeans clustering

---
+++ src/ml/customer_segmentation.py
@@ -0,0 +1,57 @@
+from unittest import result
+
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import col, when
+
+from pyspark.ml.feature import VectorAssembler
+from pyspark.ml.clustering import KMeans
+
+spark = SparkSession.builder.getOrCreate()
+
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--catalog")
+parser.add_argument("--schema")
+args = parser.parse_args()
+
+catlog = args.catalog
+schema = args.schema
+
+gold_orders_df = spark.read.table(f"{catlog}.{schema}.gold_orders")
+
+feature_df = gold_orders_df.select("total_orders", "total_spent", "avg_order_value", "days_since_last_order").dropna()
+
+
+# Converting to features vector for ML model input
+assembler = VectorAssembler(
+    inputCols=["total_orders", "total_spent", "avg_order_value", "days_since_last_order"],
+    outputCol="features"
+)
+final_df = assembler.transform(feature_df)
+
+
+# KMeans clustering for customer segmentation
+kmeans = KMeans(k=3, seed=42)
+model = kmeans.fit(final_df)
+clustered_df = model.transform(final_df)
+
+
+# Joining clustered_df with gold_orders_df to get customer_id and city back in the final output
+result_df = gold_orders_df.join(clustered_df.select("features", "prediction"), on="features", how="inner")
+
+
+# Mapping cluster predictions to meaningful segment labels
+result_df = result_df.withColumn(
+    "customer_cluster",
+    when(col("prediction") == 0, "Low Value")
+    .when(col("prediction") == 1, "Medium Value")
+    .otherwise("High Value")
+)
+
+
+result_df.write.mode("overwrite").option("overwriteSchema", "true").saveAsTable(f"{catlog}.{schema}.customer_segments")
+
+print("✅ Customer segmentation completed...!")
+
+
